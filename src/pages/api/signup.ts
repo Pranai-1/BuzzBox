289feb91109e6858a4097052000e0a0523145c41
Pranai-1 <reddypranai2017@gmail.com>
2023-10-13 12:21:35 +0530

    vercel

--- src/pages/api/signup.ts
+++ src/pages/api/signup.ts
@@ -14,11 +14,11 @@ const userInput = z.object({
 export default async function Handler(req: NextApiRequest, res: NextApiResponse) {
     const prisma = new PrismaClient();
     const body: signupBody = req.body;
-    const parsedInput = userInput.safeParse(body);
+    // const parsedInput = userInput.safeParse(body);
   
-    if (!parsedInput.success) {
-      return res.status(422).json({ message: "Validation failed" });
-    }
+    // if (!parsedInput.success) {
+    //   return res.status(422).json({ message: "Validation failed" });
+    // }
   
     res.status(200).json({message:"success"})
 
