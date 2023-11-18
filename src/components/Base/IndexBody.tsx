8d67056f5b91f086ea59ffcbe339e65c5d1a3efb
Pranai-1 <reddypranai2017@gmail.com>
2023-11-18 10:30:58 +0530

    fixed any

--- src/components/Base/IndexBody.tsx
+++ src/components/Base/IndexBody.tsx
@@ -5,8 +5,8 @@ export default function IndexBody(){
     return(
         <>
         <div className="text-gray-300 w-full  h-max text-xl flex flex-col justify-center items-center font-normal  absolute top-56 md:text-black mt-3 ">
-      <p> Experience an exciting free Omegle text alternative to Find friends, connect with strangers, and</p>
-       <p> talk with girls and guys in random anonymous chat rooms. No registration required. </p>
+      <p> Experience an exciting free texting alternative to Find friends, connect with strangers, and</p>
+       <p> talk with your family and friends in random anonymous chat rooms. No registration required. </p>
 
        <div className="w-full   flex justify-center items-center gap-10 mt-8">
         <button
@@ -24,7 +24,7 @@ export default function IndexBody(){
    <div className="mt-3 bg-gradient-to-b  from-teal-500 to-purple-600 md:bg-none md:mt-32 flex flex-wrap flex-col justify-center items-center">
     <p className="text-blue-800 text-lg px-2 bg-purple-400 font-medium rounded-xl w-max mt-2">Reach people like you</p>
     <p className="text-slate-200 text-2xl  font-medium rounded-xl mt-3"> Anonymous Chat, Meet new people</p>
-    <p className=" text-gray-300 text-lg  font-medium rounded-xl mt-3">Find female and male strangers worldwide, the new modern Omegle
+    <p className=" text-gray-300 text-lg  font-medium rounded-xl mt-3">Find female and male strangers worldwide, the new modern texting
      alternative. </p>
     <p className="text-gray-300 text-lg  font-medium rounded-xl ">Connect with real people, enjoy ad free chats, and build 
     genuine relationships.</p>
