7ea1226b869902744596e9ea218238184167c18a
Pranai-1 <reddypranai2017@gmail.com>
2024-03-08 12:54:23 +0530

    fixed props

--- src/pages/api/room/getRooms.ts
+++ src/pages/api/room/getRooms.ts
@@ -7,7 +7,7 @@ export default async function handler(req:NextApiRequest,res:NextApiResponse){
  const id=Number(req.headers["id"])
 
     try{
-    const user=await prisma.user.findUnique({
+    const user=await prisma.user.findFirst({
         where:{
             id
         }
