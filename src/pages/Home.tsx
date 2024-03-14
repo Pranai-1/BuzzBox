56fbfde94b1a79ffa8759ef5e79205edcffa6996
Pranai-1 <reddypranai2017@gmail.com>
2024-03-14 16:30:34 +0530

    rooms and chats fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -27,9 +27,9 @@ import SetContacts from "@/components/SetContacts";
 import getContacts from "./api/helpers/getContacts";
 import getRooms from "./api/helpers/getRooms";
 import { useSession } from "next-auth/react";
-//const ENDPOINT="https://buzzbox-socket.onrender.com/"
+const ENDPOINT="https://buzzbox-socket.onrender.com/"
 
- const ENDPOINT="http://localhost:4000/"
+ //const ENDPOINT="http://localhost:4000/"
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const session = await getServerAuthSession(ctx);
